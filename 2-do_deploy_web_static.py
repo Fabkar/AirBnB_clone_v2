640e36000cafbe398f0ee07f610fb562d2e620fc
Fabkar <2003@holbertonschool.com>
2020-12-22 20:27:45 -0500

    update with permission su

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-# Fabric script (based on the file 1-pack_web_static.py)
+"""Fabric script (based on the file 1-pack_web_static.py)
+that distributes an archive to your web servers, using do_deploy"""
 from os.path import exists
 from fabric.api import env, put, run
 
@@ -9,23 +10,23 @@ env.hosts = ["35.196.29.78", "35.231.126.72"]
 
 def do_deploy(archive_path):
     """Distributes an archive to a web server"""
-    if not path.exists(archive_path):
+    if not exists(archive_path):
         return False
-    file_ = archive_path.split("/")[0]
-    name = file_.split(".")[1]
+    data_releases = "data/web_static/releases"
+    file_ = archive_path.split("/")[-1]
+    name = file_.split(".")[0]
 
     try:
         put(archive_path, "/tmp/".format(file_))
-        run("rm -rf /data/web_static/releases/{}".format(name))
-        run("mkdir -p /data/web_static/releases/{}".format(name))
-        run("tar -xzf /tmp/{} -C /data/web_static/release/{}/".
-            format(file_, name))
-        run("rm /tmp/{}".format(file_))
-        run("mv /data/web_static/releases/{}/web_static/* "
-            "/data/web_static/releases/{}/".format(name, name))
-        run("rm -rf /data/web_static/releases/{}/web_static".format(name))
-        run("rm -rf /data/web_static/current")
-        run("ln -s /data/web_static/releases/{}/ /data/web_static/current".
+        run("sudo rm -rf /data_releases/{}".format(name))
+        run("sudo mkdir -p /data_releases/{}".format(name))
+        run("sudo tar -xzf /tmp/{} -C /data_releases/{}/".format(file_, name))
+        run("sudo rm /tmp/{}".format(file_))
+        run("sudo mv /data_releases/{}/web_static/* "
+            "/data_releases/{}/".format(name, name))
+        run("sudo rm -rf /data_releases/{}/web_static".format(name))
+        run("sudo rm -rf /data/web_static/current")
+        run("sudo ln -s /data_releases/{}/ /data/web_static/current".
             format(name))
         return True
     except Exception:
