56a3f1aa9fed481f6cf8c921aaf6cab8fb89b156
Fabkar <2003@holbertonschool.com>
2020-12-22 20:04:20 -0500

    update

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -8,6 +8,6 @@ sudo mkdir -p /data/web_static/shared
 sudo mkdir -p /data/web_static/releases/test
 sudo echo "Nginx Configuration" | sudo tee /data/web_static/releases/test/index.html
 sudo ln -s -f /data/web_static/releases/test/ /data/web_static/current
-sudo chown -R ubuntu:ubuntu /data/
-sudo sed -i '/listen 80 default_server/a location /hbnb_static/ { alias /data/web_static/current/;}' /etc/nginx/sites-avaliable/default
+sudo chown -hR ubuntu:ubuntu /data/
+sudo sed -i '/listen 80 default_server/a location /hbnb_static/ { alias /data/web_static/current/;}' /etc/nginx/sites-available/default
 sudo service nginx restart
